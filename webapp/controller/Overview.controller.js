da62d5d1e1496ce475770ac7deaf855e4b36be2c
Jhony Gautam <lakshita.flow@gmail.com>
2025-12-03 16:18:36 +0000

    15. Adding JSONModel + Aggregation Binding + Element Binding

--- webapp/controller/Overview.controller.js
+++ webapp/controller/Overview.controller.js
@@ -1,14 +1,20 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-    "sap/ui/core/syncStyleClass"
+    "sap/ui/core/syncStyleClass",
+    "sap/ui/model/json/JSONModel"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, syncStyleClass) {
+    function (Controller, syncStyleClass, JSONModel) {
         "use strict";
 
         return Controller.extend("demo.jitu.prac.controller.Overview", {
+
+            onInit: function () {
+                var oModel = new JSONModel();
+                this.getView().setModel(oModel, "customer");
+            },
 
             onSave: function () {
                 if (!this.pDialog) {
@@ -23,9 +29,14 @@ sap.ui.define([
                     oDialog.open();
                 });
             },
-            
+
             onCloseDialog: function () {
                 this.byId("dialog").close();
+            },
+
+            onCustomerChange: function (oEvent) {
+                var oBindingContext = oEvent.getParameter("listItem").getBindingContext();
+                this.byId("bookingTable").setBindingContext(oBindingContext);
             }
         });
     });
